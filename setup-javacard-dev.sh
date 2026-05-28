1fbc22d23c70959d45108b72bd03a0ef9c4fe3fa
= <joel@j2i.net>
2026-05-28 13:07:54 -0400

    building PCSC

--- setup-javacard-dev.sh
+++ setup-javacard-dev.sh
@@ -3,6 +3,9 @@ echo "Setting up JavaCard development environment...";
 echo "This script will install the JavaCard SDK and Simulator on your system.";
 echo "Find more documentation at https://docs.oracle.com/en/java/javacard/3.2/";
 
+
+
+
 #Path to the Java JDK download from https://www.oracle.com/java/technologies/downloads/#java25
 javaJdkZip="/home/j2inet/Downloads/jdk-25_linux-x64_bin.tar.gz";
 #Path to the directory where you want to install the Java JDK
@@ -11,6 +14,8 @@ javaJdkInstallDir="/home/j2inet/shares/sdks/java_jdk";
 echo "Installing Java JDK from $javaJdkZip to $javaJdkInstallDir";
 mkdir -p $javaJdkInstallDir;
 tar -xzf "$javaJdkZip" -C "$javaJdkInstallDir" --strip-components=1;
+
+
 
 
 
@@ -37,18 +42,50 @@ echo "Installing JavaCard Simulator from $javaCardSimulatorZip to $installDir";
 mkdir -p $javaCardSimulatorDir;
 tar -xzf "$javaCardSimulatorZip" -C "$javaCardSimulatorDir" --strip-components=1;
 
+
+sudo apt install build-essential autoconf automake libtool flex pkg-config libudev-dev libusb-1.0-0-dev -y
+sudo apt-get install meson  -y
+sudo apt install polkitd cmake libpolkit-gobject-1-dev libsystemd0 libsystemd-dev systemd systemd-dev doxygen -y
+# curl https://pcsclite.apdu.fr/files/pcsc-lite-2.4.1.tar.xz -o ~/Downloads/pcsc-lite-2.4.1.tar.xz
+# pcsc=~/Downloads/pcsc-lite-2.4.1.tar.xz
+# mkdir $PCSC_HOME
+# tar -xf "$pcsc" -C "$PCSC_HOME" --strip-components=1
+
+export PCSC_HOME=$installDir/PCSC
+pushd $PCSC_HOME
+git clone git@github.com:LudovicRousseau/PCSC.git source
+cd source
+meson setup builddir
+cd builddir
+meson compile
+sudo meson install
+echo "\x1b[37mBuilding!\x1b[0m"
+pwd
+#meson compile
+# sudo /usr/local/sbin/pcscd --foreground
+/usr/local/sbin/pcscd --version
+
+popd
+
+
+
+
+
 export JAVA_HOME="$javaJdkInstallDir";
 export JC_HOME="$javaCardSdkDir";
 export JC_SIMULATOR_HOME="$javaCardSimulatorDir";
+export JC_HOME_SIMULATOR="$javaCardSimulatorDir";
+export JC_HOME_TOOLS=""
 export CLASSPATH="$JC_HOME/lib/api_classic-3.2.0.jar:$JC_HOME/lib/tools.jar:$JC_SIMULATOR_HOME/lib/simulator.jar";
-export PATH="$JAVA_HOME/bin:${JC_SIMULATOR_HOME}/bin:$PATH";
+export PATH="$JAVA_HOME/bin:${JC_SIMULATOR_HOME}:${JC_SIMULATOR_HOME}/bin:$PATH";
 
 export JC_HOME_TOOLS="$javaCardSdkDir";
 ls  "$JAVA_HOME/bin";
 ls "$JC_HOME"
 ls "$JC_HOME_TOOLS"
-
-cd src
-javac WalletApplet.java
+echo $PCSC_HOME
+ls $PCSC_HOME
+#cd src
+#javac WalletApplet.java
 
 
